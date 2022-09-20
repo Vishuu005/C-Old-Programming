dfde5db4bfb7b9dbfc06ac89a21cd54f9ea14170
Vishuu005 <67571378+Vishuu005@users.noreply.github.com>
2022-09-20 10:38:49 +0530

    Added My New Folder with Dated So, Go My Repository And check it out Also try for learn Everycode will be perfect in format
    New Folder Committed In My Programme Repository

---
+++ 22-09-2020/company eligibility.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+int main()
+{
+//    printf("RESUME\n");
+//	int distance;
+//	printf("enter your distance from company:");
+//	scanf("%d",&distance);
+//	if(distance<=5)
+//	{
+//		printf("eligible to join company\n\n");
+//	}
+//	else
+//	{
+//		printf("not eligible to join company\n\n");
+//	}
+//	int bike;
+//	printf("enter your bike mileage:");
+//	scanf("%d",& bike);
+//	if(bike>=50)
+//	{
+//		printf("yeah you can join\n\n");
+//	}
+//	else
+//	{
+//		printf("nah u cant join this company\n\n");
+//	}
+//	float height;
+//	printf("enter your height:");
+//	scanf("%f",&height);
+//	if(height>=5.11)
+//	{
+//		printf("eligible to join\n\n");
+//	}
+//	else
+//	{
+//		printf("not eligible to join\n\n");
+//	}
+//	int weight;
+//	printf("enter your weight in pounds:");
+//	scanf("%d",&weight);
+//	if(weight>=100&&weight<=200)
+//	{
+//		printf("eligible to join\n\n");
+//	}
+//	else
+//	{
+//		printf("not eligible to join\n\n");
+//	}
+	
+	char qualification[20];
+	printf("are you 12th pass:");
+	scanf("%s",&qualification);
+	if(qualification=="yes")
+	{
+		printf("yes you can");
+	}
+	else
+	{
+		printf("no u cant join");
+	}
+return 0;
+}
---
+++ 22-09-2020/income tax.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	printf("INCOME TAX\n\n");
+	int salary;
+	printf("enter your salary:");
+	scanf("%d",&salary);
+	if(salary<=500000)
+	{
+		printf("your income tax is:0");
+	}
+	float abcd, salary1;
+	if(salary1>500000&&salary1<=1000000)
+	salary1-=500000;
+	abcd=salary1*.10;
+	{printf("your income tax is:%f",abcd);
+	}
+}
